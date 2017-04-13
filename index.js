ad3ae8e11b3f65d4c65c059653a68419e94b07dc
allenjprice <allenjprice@gmail.com>
2017-04-12 20:37:16 -0700

    add request handlers for /phrases

--- index.js
+++ index.js
@@ -9,10 +9,32 @@ const server = http.createServer(function(request, response){
   console.log(`SERVING: ${request.method} to ${request.url}`);
 
   if (request.url === '/phrases') {
-    // return JSON of all the phrases
-    response.setHeader('Content-Type', 'application/json;charset=utf-8');
-    response.writeHead(200);
-    response.end(JSON.stringify(phrases));
+    // GET /phrases - return JSON of all the phrases
+    if (request.method === 'GET'){
+      response.setHeader('Content-Type', 'application/json;charset=utf-8');
+      response.writeHead(200);
+      response.end(JSON.stringify(phrases));
+    }
+
+    // POST /phrases - add a new phrase to the list
+    if (request.method === 'POST') {
+      // listen for all the data
+      let data = '';
+      request.on('data', function(chunk){
+        data += chunk;
+      });
+      // concatenate all the chunks of data together
+      request.on('end', function(){
+        let newPhrase = JSON.parse(data);
+        newPhrase.id = phrases.length;
+        phrases.push(newPhrase);
+
+        response.writeHead(201);
+        response.end(`Added ${newPhrase.korean} to the list`);
+      })
+      // add an id to the phrase (using the length of phrases)
+      // push phrase into the collection of phrases
+    }
   }
   console.log('the server heard a request');
   response.end('here is the response');
