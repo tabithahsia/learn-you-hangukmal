93e876829f1db3003ba17fcea2a849268b331007
allenjprice <allenjprice@gmail.com>
2017-04-12 21:49:37 -0700

    add client support for getting all phrases

--- client/app.js
+++ client/app.js
@@ -1,1 +1,24 @@
 console.log('learn you korean client is running...');
+
+$('#show-all').on('click', function(){
+  $.ajax({
+    method: 'GET',
+    url: 'http://localhost:1337/phrases'
+  })
+  .done(function(phrases){
+    renderPhrases(phrases);
+  });
+});
+
+function renderPhrases(phrases) {
+  $('#phrases').html('');
+  phrases.forEach((phrase) => {
+    renderPhrase(phrase);
+  });
+}
+
+function renderPhrase(phrase) {
+  var $el = $('<div class="phrase"></div>');
+  $el.text(`English: ${phrase.english} || Korean: ${phrase.korean}`);
+  $('#phrases').append($el);
+}
