98d1bb76c5bcc5d449dbf36f64a995df990480c7
Tabitha Hsia <tabithahsia@gmail.com>
2017-04-18 20:53:12 -0700

    set up handler for GET

--- index.js
+++ index.js
@@ -1,65 +1,16 @@
-const http = require('http');
-const fs = require('fs');
+const express = require('express');
 
 const phrases = require('./data/phrases');
 
 const PORT = 1337;
-const IP = '127.0.0.1'
+const IP = '127.0.01';
+const app = express();
 
-const server = http.createServer(function(request, response){
-  console.log(`SERVING: ${request.method} to ${request.url}`);
-
-  if (request.url === '/phrases') {
-    // GET /phrases - return JSON of all the phrases
-    if (request.method === 'GET'){
-      response.setHeader('Content-Type', 'application/json;charset=utf-8');
-      response.writeHead(200);
-      response.end(JSON.stringify(phrases));
-    }
-
-    // POST /phrases - add a new phrase to the list
-    if (request.method === 'POST') {
-      // listen for all the data
-      let data = '';
-      request.on('data', function(chunk){
-        data += chunk;
-      });
-      // concatenate all the chunks of data together
-      request.on('end', function(){
-        let newPhrase = JSON.parse(data);
-        // add an id to the phrase (using the length of phrases)
-        newPhrase.id = phrases.length;
-        // push phrase into the collection of phrases
-        phrases.push(newPhrase);
-
-        // close out the response cycle
-        response.writeHead(201);
-        response.end(`Added ${newPhrase.korean} to the list`);
-      });
-    }
-  } else if (request.url === '/' && request.method === 'GET') {
-    // GET / - serve up index.html
-    fs.readFile(__dirname + '/client/index.html', function(err, data){
-      if (err) { console.error(err); }
-
-      response.setHeader('Content-Type', 'text/html;charset=utf-8');
-      response.writeHead(200);
-      response.end(data);
-    });
-  } else if (request.url === '/client/app.js' && request.method === 'GET') {
-    fs.readFile(__dirname + '/client/app.js', function(err, data){
-      if (err) { console.error(err); }
-
-      response.setHeader('Content-Type', 'application/javascript;charset=utf-8');
-      response.writeHead(200);
-      response.end(data);
-    });
-  } else {
-    console.log('the server heard a request');
-    response.end('here is the response');
-  }
+app.get('/', (req, res) => { //similar to request.method === get
+  res.sendFile(__dirname + '/client/index.html');
 
 });
 
-console.log('firing up the server...');
-server.listen(PORT, IP);
+app.listen(PORT, () => {
+  console.log(`learn-you-hangukmal express edition is listening on port ${PORT}`);
+});
